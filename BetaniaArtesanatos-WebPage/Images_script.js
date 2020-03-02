86d038c12f4e1da8141c527fba882829591e49ff
Sinval de Deus Vieira Júnior <sinvalvieirajunior@gmail.com>
2020-03-01 22:14:01 -0300

    Add files via upload

---
+++ BetaniaArtesanatos-WebPage/Images_script.js
@@ -0,0 +1,22 @@
+function whenImagemClicked(image_id){
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(image_id);
+    var modalImg = document.getElementById("img02");
+    var captionText = document.getElementById("caption");
+    img.onclick=function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+ }
